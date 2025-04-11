6952a0b2071c1bf09d75e6d53e1e12f893ab9bd6
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-04-11 17:37:53 +0530

    [Recursion -> Basics started]

---
+++ 03-Recurssion/Rec_01_BasicStruc.java
@@ -0,0 +1,15 @@
+public class Rec_01_BasicStruc {
+
+    public static void recFun(int x){
+        // base condition
+
+        // your work 
+
+        // inner function call
+
+    }
+
+    public static void main(String[] args) {
+        
+    }    
+}
