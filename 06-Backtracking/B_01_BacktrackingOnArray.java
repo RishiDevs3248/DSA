736ebf80cb780c6bd44cb14a73b8c04908875a9a
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-04-29 13:30:59 +0530

    Backtracking -> Basics

---
+++ 06-Backtracking/B_01_BacktrackingOnArray.java
@@ -0,0 +1,26 @@
+public class B_01_BacktrackingOnArray {
+    public static void Change(int arr[] , int i , int val){
+        //base case
+        if(i == arr.length){
+            PrintArr(arr);
+            System.out.println();
+            return;
+        }
+        //kaam
+        arr[i] = val;
+        Change(arr, i+1, val+1);
+        arr[i] = arr[i] - 2;
+    }
+
+    public static void PrintArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        Change(arr, 0, 1);
+        PrintArr(arr);
+    }
+}
