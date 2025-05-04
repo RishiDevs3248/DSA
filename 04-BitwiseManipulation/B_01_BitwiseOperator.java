1178286f0e009c301af2ad6889a47d3c1c2032d6
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-05-04 10:14:29 +0530

    [Bitwise -> Basics]

---
+++ 04-BitwiseManipulation/B_01_BitwiseOperator.java
@@ -0,0 +1,12 @@
+public class B_01_BitwiseOperator {
+    public static void main(String[] args) {
+        //Bitwise operators 
+        // & - AND
+        // | - OR
+        // ^ - XOR
+        // ~ - NEGATION
+        // << - Left shift
+        // >> - Right shift
+
+    }    
+}
