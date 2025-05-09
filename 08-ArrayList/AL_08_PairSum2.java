bd1264f2404baf049c6b1cd30ee6c5e6ad115231
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-05-09 12:40:23 +0530

    [ArrayList - pair sun 2 ]

--- 08-ArrayList/AL_08_PairSum2.java
+++ 08-ArrayList/AL_08_PairSum2.java
@@ -3,6 +3,40 @@
 import java.util.ArrayList;
 
 public class AL_08_PairSum2 {
+
+    /*
+     * two pointer approach 
+     * first finding the pivot (the point where its rotated )
+     * the point is end of the array and the next idx is the starting point of the array
+     * updating the lp and the rp using the modulo fun "%"
+     */
+
+     public static String pairSum2(ArrayList<Integer> arr, int target){
+        int lp =0, rp =0 ;
+
+        // finding pivot 
+        for (int i = 0 ; i < arr.size()-1 ; i++){
+            if(arr.get(i)>arr.get(i+1)){
+                rp = i;  // largest element
+                lp = i+1;// smallest element
+                break;
+            }
+        }
+
+        // now comparing with the target and updating the pointer using mod fun "%"
+        while (lp != rp){
+            if ((arr.get(lp)+arr.get(rp)) == target){
+                return "Elements are "+arr.get(lp)+", "+arr.get(rp);
+            }else if ((arr.get(lp)+arr.get(rp)) < target){
+                lp = ((lp+1) % arr.size());
+            }else{
+                rp = ((rp-1+arr.size()) % arr.size());
+            }
+        }
+
+        return "Elements not found";
+     }
+
     public static void main(String[] args) {
         ArrayList<Integer> arr= new ArrayList<>();
         arr.add(7);
@@ -15,6 +49,6 @@ public class AL_08_PairSum2 {
         arr.add(5);
         arr.add(6);
 
-        
+        System.out.println(pairSum2(arr, 5));
     }
 }
