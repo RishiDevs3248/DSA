316bc8f907b8dab96b8ff5fe9c1425335d64b900
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-05-13 13:36:32 +0530

    [Linklist -> Basics2]

--- 09-Linklist/LL_01_LinklistBasics.java
+++ 09-Linklist/LL_01_LinklistBasics.java
@@ -1,9 +1,12 @@
+
 public class LL_01_LinklistBasics {
+
     public static class Node {
+
         int data;
         Node next;
 
-        public Node (int data){
+        public Node(int data) {
             this.data = data;
             this.next = null;
         }
@@ -21,14 +24,26 @@ public class LL_01_LinklistBasics {
 
 
 
+
+    // variable for storing size
+    public static int size = 0;
+
+
+
+
+
+    
     // Add first function 
     // not using a static because we are not directly calling fun in main fun , we will call it using a obj
-    public void addFirst(int data){
+    public void addFirst(int data) {
         // step 1 - create new node
         Node newNode = new Node(data);
 
+        // updating size
+        size++;
+
         // if linklist is empty
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -37,20 +52,22 @@ public class LL_01_LinklistBasics {
         newNode.next = head;
 
         // step 3 - reset head
-        head = newNode; 
+        head = newNode;
+
     }
 
 
 
 
 
+    
     // Add Last 
-    public void addLast(int data){
-         // step 1 - create new node
+    public void addLast(int data) {
+        // step 1 - create new node
         Node newNode = new Node(data);
 
         // if linklist is empty
-        if(head == null){
+        if (head == null) {
             head = tail = newNode;
             return;
         }
@@ -59,18 +76,115 @@ public class LL_01_LinklistBasics {
         tail.next = newNode;
 
         // step 3 - reset tail
-        tail = newNode;  
+        tail = newNode;
+
+        // updating size
+        size++;
     }
 
 
 
 
 
-    // Print linklist
-    public void printLL(){
+    
+    // Add in middle 
+    public void addMiddle(int data, int target) {
+        //step 1 - create a new node 
+        Node newNode = new Node(data);
+
+        //step 2 -  travling to right index 
         Node temp = head;
-        while(temp != null){
-            System.out.print(temp.data+" -> ");
+        int i = 0;
+        while ((i + 1) != target) {
+            temp = temp.next;
+            i++;
+        }
+
+        //step 3 - inserting element
+        newNode.next = temp.next;
+        temp.next = newNode;
+
+        // updating size
+        size++;
+    }
+
+
+
+
+
+    
+    // remove first
+    public void removefirst(){
+        //case 1 - size 0 
+        if(head == null){
+            System.out.println("Linklist empty!!, cannot remove");
+            return;
+        }
+
+        //case 2 - size 1
+        int data = head.data;
+        if(head == tail){
+            head = tail = null;
+            System.err.println("removed : "+data);
+            //updating size
+            size = 0;
+            return;
+        }
+
+        //case 3 - size more than 1
+        head = head.next;  // this line removes the node
+        System.err.println("removed : "+data);
+        //updating size
+        size--;
+    }
+
+
+
+
+
+    
+    //remove last
+    public void removeLast(){
+        //case 1 - size 0  
+        if(tail == null){
+            System.out.println("Linklist empty!!, cannot remove");
+            return;
+        }
+
+        //case 2 - size 1
+        int data = tail.data;
+        if(head == tail){
+            head = tail = null;
+            System.err.println("removed : "+data);
+            //updating size
+            size = 0;
+            return;
+        }
+
+        //case 3 - size more than 1
+        Node temp = head;
+         // travaling to the position before tail (n-1)
+        while (temp.next != tail){
+            temp = temp.next;
+        }
+        //updating tail and removing the link 
+        tail = temp ;
+        temp.next = null;
+        System.err.println("removed : "+data);
+        //updating size
+        size--;
+    }
+
+
+
+
+
+    
+    // Print linklist
+    public void printLL() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
         System.out.print("null");
@@ -78,6 +192,9 @@ public class LL_01_LinklistBasics {
 
 
 
+
+
+    
     public static void main(String[] args) {
         LL_01_LinklistBasics ll = new LL_01_LinklistBasics();
         ll.addFirst(3);
@@ -87,5 +204,25 @@ public class LL_01_LinklistBasics {
         ll.addLast(5);
         ll.addLast(6);
         ll.printLL();
+
+        System.out.println();
+
+        ll.addMiddle(001, 4);
+        ll.printLL();
+
+        System.out.println();
+        System.out.println("size : "+ll.size);
+
+
+        ll.removefirst();
+        ll.printLL();
+        System.out.println();
+        System.out.println("size : "+ll.size);
+
+
+        ll.removeLast();
+        ll.printLL();
+        System.out.println();
+        System.out.println("size : "+ll.size);
     }
 }
