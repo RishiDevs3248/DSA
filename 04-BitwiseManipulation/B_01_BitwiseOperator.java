8310e4a5cc237a59bf53a2ac98e5b43e54abaed4
Hrishikesh Alabnur <alabnur2004@gmail.com>
2025-05-28 12:44:33 +0530

    bitwise revision

--- 04-BitwiseManipulation/B_01_BitwiseOperator.java
+++ 04-BitwiseManipulation/B_01_BitwiseOperator.java
@@ -2,10 +2,15 @@ public class B_01_BitwiseOperator {
     public static void main(String[] args) {
         //Bitwise operators 
         // & - AND
+
         // | - OR
-        // ^ - XOR
+
+        // ^ - XOR  - you can use this one for finding single element in array of duplicate values (O(logn))
+
         // ~ - NEGATION
+
         // << - Left shift
+
         // >> - Right shift
     }    
 }
